7156234f8b1d3ec4e84f8711d4ad163fb750ad11
Efrain Ospino <efrainospino14@gmail.com>
2022-06-27 16:04:36 -0500

    Docs creado

---
+++ docs/precache-manifest.15f254cfc121c423d09e99687292f910.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b50ca5f1a65b32b521e7",
+    "url": "/static/css/main.23f1fc9a.chunk.css"
+  },
+  {
+    "revision": "b50ca5f1a65b32b521e7",
+    "url": "/static/js/main.b50ca5f1.chunk.js"
+  },
+  {
+    "revision": "46813f8bd4537e98c1cf",
+    "url": "/static/css/1.12125b8c.chunk.css"
+  },
+  {
+    "revision": "46813f8bd4537e98c1cf",
+    "url": "/static/js/1.46813f8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "222913ebd0bcada3bdcdf4612fc6d139",
+    "url": "/static/media/Logo2.222913eb.png"
+  },
+  {
+    "revision": "d903407e6502218512419ec0d430f3a3",
+    "url": "/static/media/Efrain ospino - hoja de vida.d903407e.pdf"
+  },
+  {
+    "revision": "3851e0194e82efa8a32bdc79af7de5a1",
+    "url": "/static/media/pexels-pavel-danilyuk-5495784 (1).3851e019.mp4"
+  },
+  {
+    "revision": "0489dd5bd938adc57570775c095b2b2a",
+    "url": "/static/media/Foto hoja de vida.0489dd5b.jpeg"
+  },
+  {
+    "revision": "1e3fd2f633508cf57feaf023a658980e",
+    "url": "/static/media/Preloader.1e3fd2f6.mp4"
+  },
+  {
+    "revision": "0eeeaf978fbb527f48c0cf21b6395996",
+    "url": "/index.html"
+  }
+];
