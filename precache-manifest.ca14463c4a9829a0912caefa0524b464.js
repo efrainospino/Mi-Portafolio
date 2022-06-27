e22c3c8953ffe7570be084de4a3a05df6e3f4fbb
Efrain Ospino <efrainospino14@gmail.com>
2022-06-27 16:46:16 -0500

    Updates

---
+++ precache-manifest.ca14463c4a9829a0912caefa0524b464.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b50ca5f1a65b32b521e7",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/css/main.23f1fc9a.chunk.css"
+  },
+  {
+    "revision": "b50ca5f1a65b32b521e7",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/js/main.b50ca5f1.chunk.js"
+  },
+  {
+    "revision": "46813f8bd4537e98c1cf",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/css/1.12125b8c.chunk.css"
+  },
+  {
+    "revision": "46813f8bd4537e98c1cf",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/js/1.46813f8b.chunk.js"
+  },
+  {
+    "revision": "ddc24b3e9d40a75d8694",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/js/runtime~main.ddc24b3e.js"
+  },
+  {
+    "revision": "222913ebd0bcada3bdcdf4612fc6d139",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/media/Logo2.222913eb.png"
+  },
+  {
+    "revision": "d903407e6502218512419ec0d430f3a3",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/media/Efrain ospino - hoja de vida.d903407e.pdf"
+  },
+  {
+    "revision": "3851e0194e82efa8a32bdc79af7de5a1",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/media/pexels-pavel-danilyuk-5495784 (1).3851e019.mp4"
+  },
+  {
+    "revision": "0489dd5bd938adc57570775c095b2b2a",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/media/Foto hoja de vida.0489dd5b.jpeg"
+  },
+  {
+    "revision": "1e3fd2f633508cf57feaf023a658980e",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/static/media/Preloader.1e3fd2f6.mp4"
+  },
+  {
+    "revision": "f6eb913df8520ca2966d5a2f08ef3d2b",
+    "url": "%7BGithub%20Username%7D.github.io/%7BNombreRepo%7D/index.html"
+  }
+];
